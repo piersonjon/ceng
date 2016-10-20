ad9eb0671bf5fa1082a17b87c0475fa1d35fba20
Jon Pierson <admin@kinix.net>
2016-10-20 02:42:23 -0400

    Moving this up to latest to move to a new machine.

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@ class Creature{
 	int HP, MP, Vit, Wis, Dex, Spd, Lck;
 public:
 	// this is a learning experience. we'll see if there's an easier way to make these later.
+	/*
 	int getHP();
 	void setHP(int newHP);
 	int getMP();
@@ -28,5 +29,21 @@ public:
 	void setSpd(int newSpd);
 	int getLck();
 	void setLck(int newLck);
-	
+	*/ // removing these for now. trying to use get/setStat to remove the amount of whitespace.
+
+	int getStat(int statID);
+	void setStat(int statID, int newVal);
+
 };
+
+int Creature::getStat(int statID) {
+	switch(statID) {
+		case 1: return HP;
+		case 2: return MP;
+		case 3: return Vit;
+		case 4: return Wis;
+		case 5: return Dex;
+		case 6: return Spd;
+		case 7: return Lck;
+	}
+}
